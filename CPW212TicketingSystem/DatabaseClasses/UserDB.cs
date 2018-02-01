b337af27598c9e279d396f1e8024838a88559378
Alex-Ram <Alex-Ram@users.noreply.github.com>
2018-02-01 13:10:50 -0800

    Commiting login checker, Register function (still neesd to be hooked up through the form and working on the client side

---
+++ CPW212TicketingSystem/DatabaseClasses/UserDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPW212TicketingSystem.DatabaseClasses
+{
+    static class UserDB
+    {
+        public static User FindUser(string Username, string Password)
+        {
+            // stores our dbcontext "our tracker" for our database into a variable so can act upon it.
+            var db = new TicketingSystemDBContext();
+            #region Internal Learning notes
+            // uses lambda expression to..
+            // return 1 resault or throw an exception if more than 1 result is obtain
+            // so it checks the db.users which is basically stating check our db context which is tracking our database
+            // then check out the dbset within that db context which will look at the users table essentially as the 
+            // dbset is that entity.
+            // then it checks to see if it matches the username and password parameter that is passed in.
+            // then finally if more then 1 row is returned it will throw the exception if not 1 row is returned
+            // then we know we have a user with that matching authentication.
+            #endregion
+            // we can omit the where clause as single or default implicitly should only return 1 result
+            User userLoginCheck = db.Users.SingleOrDefault(currUser => currUser.Username == Username && currUser.Password == Password);
+
+            return userLoginCheck;
+        }
+
+        public static void RegisterUser(User regUser)
+        {
+            var db = new TicketingSystemDBContext();
+
+            db.Users.Add(regUser);
+        }
+
+    }
+}
--- CPW212TicketingSystem/Forms/FrmLogin.cs
+++ CPW212TicketingSystem/Forms/FrmLogin.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CPW212TicketingSystem.DatabaseClasses;
+using CPW212TicketingSystem.Forms;
 
 namespace CPW212TicketingSystem
 {
@@ -15,6 +17,39 @@ namespace CPW212TicketingSystem
         public FrmLogin()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // this will check user authentication
+            User userToLogin = UserDB.FindUser(txtUserName.Text, txtPassWord.Text);
+
+            if (userToLogin != null)
+            {
+                // this will hide the login form and let us now just browse the main form. 
+                // probably not the best way to do this I will look into better ways to handle this.
+                // as the login form is pretty much useless after a user is authenticated.
+
+
+                // after further testing, if we exit out of the main form the program is still running
+                // as login is still running in the back ground. 
+                // TODO: Either create a method to be called once main form is exited to exit login ( which would be the messier way imo)
+                // TODO: Second option would be to research a way to do it more cleanly maybe have Application.Run point somewhere else??
+                this.Hide();
+                FrmMain mainForm = new FrmMain();
+                mainForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("User Authentication Failed");
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            // TODO: create a query to input a new user into the database.
+            // everyone will have to default at the lowest level. and then at a certain level
+            // another user at a higher tier level can then promote that person up the ranks.
         }
     }
 }
