740867c7d97a8e105edf15c9b38df93ea0bf767a
Chad <chadd_84@yahoo.com>
2018-02-09 14:18:57 -0800

    FrmRegistration adds user to database.

---
+++ CPW212TicketingSystem/Forms/FrmRegistration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPW212TicketingSystem
+{
+    public partial class FrmRegistration : Form
+    {
+        public FrmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            User user = new User()
+            {
+                FirstName = txtFirstName.Text,
+                LastName = txtLastName.Text,
+                Username = txtUsername.Text,
+                Password = txtPassword.Text
+            };
+
+            if (UserDB.Add(user))
+            {
+                MessageBox.Show("Registration succesful");
+            }
+            else
+            {
+                MessageBox.Show("Error: Registration not succesful");
+            }
+        }
+    }
+}
