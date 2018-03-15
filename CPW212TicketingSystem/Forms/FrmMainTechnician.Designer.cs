89ca25889ddef59386c0b8568a37ca1d65a59b13
liljk <liljk@users.noreply.github.com>
2018-03-15 09:59:26 -0700

    Editing more layout

--- CPW212TicketingSystem/Forms/FrmMainTechnician.Designer.cs
+++ CPW212TicketingSystem/Forms/FrmMainTechnician.Designer.cs
@@ -39,14 +39,17 @@
             this.BtnEditUsers = new System.Windows.Forms.Button();
             this.BtnEditRoles = new System.Windows.Forms.Button();
             this.BtnEditPriorities = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BtnCreateTicket
             // 
-            this.BtnCreateTicket.Location = new System.Drawing.Point(332, 598);
+            this.BtnCreateTicket.FlatAppearance.BorderSize = 0;
+            this.BtnCreateTicket.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCreateTicket.Location = new System.Drawing.Point(647, 370);
             this.BtnCreateTicket.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.BtnCreateTicket.Name = "BtnCreateTicket";
-            this.BtnCreateTicket.Size = new System.Drawing.Size(211, 57);
+            this.BtnCreateTicket.Size = new System.Drawing.Size(306, 186);
             this.BtnCreateTicket.TabIndex = 15;
             this.BtnCreateTicket.Text = "Create New Ticket";
             this.BtnCreateTicket.UseVisualStyleBackColor = true;
@@ -55,7 +58,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 552);
+            this.label3.Location = new System.Drawing.Point(71, 511);
             this.label3.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 25);
@@ -65,7 +68,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 506);
+            this.label2.Location = new System.Drawing.Point(71, 453);
             this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(108, 25);
@@ -76,7 +79,7 @@
             // 
             this.CboPriorities.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.CboPriorities.FormattingEnabled = true;
-            this.CboPriorities.Location = new System.Drawing.Point(142, 546);
+            this.CboPriorities.Location = new System.Drawing.Point(195, 506);
             this.CboPriorities.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.CboPriorities.Name = "CboPriorities";
             this.CboPriorities.Size = new System.Drawing.Size(396, 33);
@@ -84,7 +87,7 @@
             // 
             // DtpDueDate
             // 
-            this.DtpDueDate.Location = new System.Drawing.Point(142, 500);
+            this.DtpDueDate.Location = new System.Drawing.Point(195, 450);
             this.DtpDueDate.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.DtpDueDate.Name = "DtpDueDate";
             this.DtpDueDate.Size = new System.Drawing.Size(396, 31);
@@ -92,7 +95,7 @@
             // 
             // TxtTitle
             // 
-            this.TxtTitle.Location = new System.Drawing.Point(142, 446);
+            this.TxtTitle.Location = new System.Drawing.Point(195, 394);
             this.TxtTitle.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.TxtTitle.Name = "TxtTitle";
             this.TxtTitle.Size = new System.Drawing.Size(396, 31);
@@ -101,7 +104,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 452);
+            this.label1.Location = new System.Drawing.Point(71, 397);
             this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 25);
@@ -112,20 +115,23 @@
             // 
             this.LstTickets.FormattingEnabled = true;
             this.LstTickets.ItemHeight = 25;
-            this.LstTickets.Location = new System.Drawing.Point(24, 23);
+            this.LstTickets.Location = new System.Drawing.Point(24, 92);
             this.LstTickets.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.LstTickets.Name = "LstTickets";
-            this.LstTickets.Size = new System.Drawing.Size(1196, 379);
+            this.LstTickets.Size = new System.Drawing.Size(912, 254);
             this.LstTickets.TabIndex = 8;
             this.LstTickets.SelectedIndexChanged += new System.EventHandler(this.LstTickets_SelectedIndexChanged);
             this.LstTickets.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.LstTickets_MouseDoubleClick);
             // 
             // BtnEditUsers
             // 
-            this.BtnEditUsers.Location = new System.Drawing.Point(952, 446);
+            this.BtnEditUsers.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnEditUsers.FlatAppearance.BorderSize = 0;
+            this.BtnEditUsers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnEditUsers.Location = new System.Drawing.Point(13, 568);
             this.BtnEditUsers.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.BtnEditUsers.Name = "BtnEditUsers";
-            this.BtnEditUsers.Size = new System.Drawing.Size(200, 96);
+            this.BtnEditUsers.Size = new System.Drawing.Size(270, 168);
             this.BtnEditUsers.TabIndex = 16;
             this.BtnEditUsers.Text = "Edit Users";
             this.BtnEditUsers.UseVisualStyleBackColor = true;
@@ -133,10 +139,13 @@
             // 
             // BtnEditRoles
             // 
-            this.BtnEditRoles.Location = new System.Drawing.Point(952, 554);
+            this.BtnEditRoles.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnEditRoles.FlatAppearance.BorderSize = 0;
+            this.BtnEditRoles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnEditRoles.Location = new System.Drawing.Point(286, 568);
             this.BtnEditRoles.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.BtnEditRoles.Name = "BtnEditRoles";
-            this.BtnEditRoles.Size = new System.Drawing.Size(200, 96);
+            this.BtnEditRoles.Size = new System.Drawing.Size(271, 168);
             this.BtnEditRoles.TabIndex = 17;
             this.BtnEditRoles.Text = "Edit Roles";
             this.BtnEditRoles.UseVisualStyleBackColor = true;
@@ -144,20 +153,34 @@
             // 
             // BtnEditPriorities
             // 
-            this.BtnEditPriorities.Location = new System.Drawing.Point(952, 661);
+            this.BtnEditPriorities.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnEditPriorities.FlatAppearance.BorderSize = 0;
+            this.BtnEditPriorities.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnEditPriorities.Location = new System.Drawing.Point(560, 568);
             this.BtnEditPriorities.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.BtnEditPriorities.Name = "BtnEditPriorities";
-            this.BtnEditPriorities.Size = new System.Drawing.Size(200, 96);
+            this.BtnEditPriorities.Size = new System.Drawing.Size(273, 168);
             this.BtnEditPriorities.TabIndex = 18;
             this.BtnEditPriorities.Text = "Edit Priorities";
             this.BtnEditPriorities.UseVisualStyleBackColor = true;
             this.BtnEditPriorities.Click += new System.EventHandler(this.BtnEditPriorities_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(843, 657);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 79);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // FrmMainTechnician
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1248, 848);
+            this.ClientSize = new System.Drawing.Size(958, 752);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.BtnEditPriorities);
             this.Controls.Add(this.BtnEditRoles);
             this.Controls.Add(this.BtnEditUsers);
@@ -169,8 +192,10 @@
             this.Controls.Add(this.TxtTitle);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.LstTickets);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.Name = "FrmMainTechnician";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmMainTechnician";
             this.Load += new System.EventHandler(this.FrmMainTechnician_Load);
             this.ResumeLayout(false);
@@ -191,5 +216,6 @@
         private System.Windows.Forms.Button BtnEditUsers;
         private System.Windows.Forms.Button BtnEditRoles;
         private System.Windows.Forms.Button BtnEditPriorities;
+        private System.Windows.Forms.Button button1;
     }
 }
--- CPW212TicketingSystem/Forms/FrmMainTechnician.cs
+++ CPW212TicketingSystem/Forms/FrmMainTechnician.cs
@@ -106,5 +106,10 @@ namespace CPW212TicketingSystem.Forms
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
